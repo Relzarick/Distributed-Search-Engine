256ea5e4409f9b1462c2ed87d11160c20d399e6c
Exodus <junfeng.nyo@gmail.com>
2026-06-28 19:54:35 +0800

    Updated test coverage for typeConverter & standardTokenization methods.

--- backend/src/test/java/tokenizer/StandardTokenizationTest.java
+++ backend/src/test/java/tokenizer/StandardTokenizationTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 
 class StandardTokenizationTest {
     @Test
     void basicSentenceTest() {
         var tokenizer = new StandardTokenization();
+
+        assertInstanceOf(List.class, tokenizer.toTokens("This, is a test!"));
         assertEquals(List.of("test"), tokenizer.toTokens("This, is a test!"));
+
     }
 
     @Test
