8fb2834449328e2379ce9fef2e20898e6dbab502
Exodus <junfeng.nyo@gmail.com>
2026-06-28 19:52:34 +0800

    Revert "Updated test coverage for typeConverter method"
    This reverts commit 560e5df216d8f9b700769f183ab0062faa8cc892.

--- backend/src/test/java/etl/TypeConverterTest.java
+++ backend/src/test/java/etl/TypeConverterTest.java
@@ -2,41 +2,30 @@ package etl;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TypeConverterTest {
     @Test
     void convertNullOrEmpty() {
         assertNull(TypeConverter.convert(null));
         assertNull(TypeConverter.convert(""));
-        assertNull(TypeConverter.convert(" "));
     }
 
     @Test
     void convertToInt() {
         assertEquals(100, TypeConverter.convert("100"));
         assertEquals(-100, TypeConverter.convert("-100"));
-
-        assertInstanceOf(Integer.class, TypeConverter.convert("2147483647"));
-        assertEquals(2147483647, TypeConverter.convert("2147483647"));
-
-        assertInstanceOf(Integer.class, TypeConverter.convert("-2147483648"));
-        assertEquals(-2147483648, TypeConverter.convert("-2147483648"));
     }
 
     @Test
     void convertToLong() {
-        assertInstanceOf(Long.class, TypeConverter.convert("2147483648"));
         assertEquals(2147483648L, TypeConverter.convert("2147483648"));
-
-        assertInstanceOf(Long.class, TypeConverter.convert("-2147483649"));
         assertEquals(-2147483649L, TypeConverter.convert("-2147483649"));
     }
 
     @Test
     void convertToDouble() {
-        assertInstanceOf(Double.class, TypeConverter.convert("100.99"));
-
         assertEquals(100.99, TypeConverter.convert("100.99"));
         assertEquals(-100.99, TypeConverter.convert("-100.99"));
     }
