b8e4d75f7a11a98577221c027940e7f3ce2c8dd4
Exodus <junfeng.nyo@gmail.com>
2026-08-03 20:27:59 +0800

    Fixed page function.

--- frontend/functions/search.js
+++ frontend/functions/search.js
@@ -1,12 +1,14 @@
 export async function onRequest(context) {
   const originalRequest = context.request;
   const originalUrl = new URL(originalRequest.url);
-
   const targetUrl = `https://service.relzarick.com/search${originalUrl.search}`;
+
+  const newHeaders = new Headers(originalRequest.headers);
+  newHeaders.delete("Host");
 
   const proxyRequest = new Request(targetUrl, {
     method: originalRequest.method,
-    headers: originalRequest.headers,
+    headers: newHeaders,
   });
 
   return fetch(proxyRequest);
